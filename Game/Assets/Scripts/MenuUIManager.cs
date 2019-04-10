d68bf1e36361d92d449e4bd10b7efb997facc835
Sebastian Majewski <majewski.sebastian1996@gmail.com>
2019-04-10 11:32:07 +0200

    Dodanie szkieletu menu głównego

---
+++ Game/Assets/Scripts/MenuUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIManager : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
