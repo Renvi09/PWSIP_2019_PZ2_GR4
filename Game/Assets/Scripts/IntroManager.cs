f665c3e0acf368ea16c9c1dbcf612316f68fef95
Sebastian Majewski <majewski.sebastian1996@gmail.com>
2019-04-20 20:03:05 +0200

    Wstępne Intro i creditsy V2

---
+++ Game/Assets/Scripts/IntroManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class IntroManager : MonoBehaviour
+{
+    public float speed = 15;
+
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        Vector3 localUp = transform.TransformDirection(0, 1, 0);
+
+        position += localUp * speed * Time.deltaTime;
+        transform.position = position;
+    }
+}
